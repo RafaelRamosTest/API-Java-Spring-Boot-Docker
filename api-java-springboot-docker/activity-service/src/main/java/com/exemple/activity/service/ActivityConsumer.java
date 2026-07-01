0b5c1ec002b352976f9ee203e87eef4203c3872d
RafaelRamosTest <rafaeldickinson@hotmail.com>
2026-07-01 20:18:52 -0300

    ajuste CI e kafka

--- api-java-springboot-docker/activity-service/src/main/java/com/exemple/activity/service/ActivityConsumer.java
+++ api-java-springboot-docker/activity-service/src/main/java/com/exemple/activity/service/ActivityConsumer.java
@@ -1,9 +1,12 @@
 package com.exemple.activity.service;
 
 import com.exemple.activity.dto.ActivityResponse;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ActivityConsumer {
@@ -13,12 +16,15 @@ public class ActivityConsumer {
     @KafkaListener(topics = "activities-topic", groupId = "activity-group")
     public void consume(String message) {
         try {
-            // Converte o JSON recebido em objeto ActivityResponse
-            ActivityResponse activity = mapper.readValue(message, ActivityResponse.class);
+            // Altere usando o TypeReference para manter o tipo genérico correto da lista
+            List<ActivityResponse> activities = mapper.readValue(message, new TypeReference<List<ActivityResponse>>() {});
 
-            // Imprime os campos de forma estruturada
-            System.out.println("Nova atividade recebida:");
-            System.out.println("Título: " + activity.getTitle());
+            System.out.println("Pacote de atividades recebido. Total: " + activities.size());
+
+            for (ActivityResponse activity : activities) {
+                System.out.println("Nova atividade recebida:");
+                System.out.println("Título: " + activity.getTitle());
+            }
 
         } catch (Exception e) {
             System.err.println("Erro ao deserializar mensagem: " + message);
